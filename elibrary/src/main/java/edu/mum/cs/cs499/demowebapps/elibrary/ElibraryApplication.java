c6558413c38f2a6e8eebf206bfce4bf0ef79d300
gutadufera <53204963+gutadufera@users.noreply.github.com>
2020-07-31 02:03:06 -0500

    my lab7 assignment

---
+++ elibrary/src/main/java/edu/mum/cs/cs499/demowebapps/elibrary/ElibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs499.demowebapps.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElibraryApplication.class, args);
+	}
+
+}
